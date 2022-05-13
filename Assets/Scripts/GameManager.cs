f69b6b4055df56317aed9a60d2cf375a21b70009
TuXimo <maxobrunetti@gmail.com>
2022-05-13 19:16:09 -0300

    F

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
